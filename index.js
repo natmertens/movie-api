6215bc45e8259f37526618f149b7575388d4152e
Schnecke <eiweala@live.com>
2021-02-26 21:55:57 +0800

    added * to cors

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ const {check, validationResult} = require('express-validator');
 
 let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', '*'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors())
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 const Models = require('./models.js');
 
