97210a0736fe12fa3420584ecea42701351cae41
Natalie Mertens <natmertens@gmx.de>
2021-07-07 07:58:52 +0200

    added allowed origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const passport = require('passport');
 require('./passport.js');
 const {check, validationResult} = require('express-validator');
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234',  'http://localhost:4200'];
 
 app.use(cors({
   origin: (origin, callback) => {
